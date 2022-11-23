73885076f29771eb26193de3c2e0a6b2bf99c5c0
madalina.toma <madalina.toma@softem.ro>
2022-11-23 20:20:56 +0200

    Add project files.

---
+++ FirstCourse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

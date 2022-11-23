2f8916230753941cedc868cba7f4c88010a581d1
madalina.toma <madalina.toma@softem.ro>
2022-11-23 21:42:52 +0200

    first app - first course

--- FirstCourse/Program.cs
+++ FirstCourse/Program.cs
@@ -6,7 +6,43 @@ namespace FirstCourse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //  Console.WriteLine("Hello World!");
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            int latime;
+            int lungime = 3;
+            Console.WriteLine("Aria unei camere");
+            Console.WriteLine("Introduceti latimea: ");
+            latime = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceri lungimea: ");
+
+            lungime = int.Parse(Console.ReadLine());
+
+            int ariaCamerei = latime * lungime;
+
+            Console.WriteLine("Aria dreptunghiului este: " + ariaCamerei);
+            //Console.ReadKey();
+
+            if (latime == lungime)
+            {
+                Console.WriteLine("camera este patrata");
+            }
+            else
+            {
+                Console.WriteLine("Camera nu este patrata");
+            }
+
+            if (latime < lungime)
+            {
+                Console.WriteLine("Lungimea este mai mare");
+            }
+            else
+            {
+                Console.WriteLine("Lungimea este mai mica");
+            }
+
+
         }
     }
 }
